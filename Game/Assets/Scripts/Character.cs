af80385993ceae2175e0bf2e3bba0793aedc3789
gws1017 <gws1017@naver.com>
2023-08-09 15:37:11 +0900

    적 플레이어 추적 구현
    - 기존 플레이어가 사용하던 Character 스크립트를 재사용한다. 스크립트를 사용하는 GameObject가 플레이어인지 아닌지 구분해서 코드를 다르게 적용함
    - 적은 navmeshagent를 이용해서 플레이어 Tag를 가진 object를 추적한다.
    - 적은 경사진 길은 이동할  수 없도록 navmesh를 bake했다
    - 플레이어의 애니메이션 컨트롤러를 상속하여 재사용하는 오버라이드 컨트롤러를 이용해서 적의 컨트롤러로 사용한다.
    - 적의 footstep 이펙트 재생은 animation event를 이용해 발이 땅에 닿을 때 재생되도록 함

--- Game/Assets/Scripts/Character.cs
+++ Game/Assets/Scripts/Character.cs
@@ -14,17 +14,32 @@ public class Character : MonoBehaviour
     public float MoveSpeed = 5f;
     public float Gravity = -9.8f;
 
+    //Enemy
+    private UnityEngine.AI.NavMeshAgent Agent;
+    private Transform TargetPlayer;
+
+    public bool IsPlayer = true;
     private void Awake()
     {
         cc = GetComponent<CharacterController>();
-        playerinput = GetComponent<PlayerInput>();
         anim = GetComponent<Animator>();
+        
+        if(!IsPlayer)
+        {
+            Agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            TargetPlayer = GameObject.FindWithTag("Player").transform;
+            Agent.speed = MoveSpeed;
+        }
+        else 
+        {
+            playerinput = GetComponent<PlayerInput>();
+        }
     }
 
-    private void CaculatePlayerMovement()
+    private void CalculatePlayerMovement()
     {
         MovementVelocity.Set(playerinput.HorizonItalInput, 0f, playerinput.VerticalInput);
-        MovementVelocity.Normalize(); //안하면 대각선 이동속도가 빨라짐
+        MovementVelocity.Normalize();
         MovementVelocity = Quaternion.Euler(0f, -45f, 0f) * MovementVelocity;
         anim.SetFloat("Speed", MovementVelocity.magnitude);
         MovementVelocity *= MoveSpeed * Time.deltaTime;
@@ -34,19 +49,38 @@ public class Character : MonoBehaviour
 
         anim.SetBool("IsFall", !cc.isGrounded);
     }
+    private void CalculateEnemyMovement()
+    {
+        if(Vector3.Distance(TargetPlayer.position, transform.position) >= Agent.stoppingDistance)
+        {
+            Agent.SetDestination(TargetPlayer.position);
+            anim.SetFloat("Speed", 0.2f);
+        }
+        else 
+        {
+            Agent.SetDestination(TargetPlayer.position);
+            anim.SetFloat("Speed", 0f);
+        }
+    }
+
     private void FixedUpdate()
     {
-        CaculatePlayerMovement();
+        if(IsPlayer)
+        {
+            CalculatePlayerMovement();
+            //중력 적용
+            if (cc.isGrounded == false)
+                VerticalVelocity = Gravity;
+            else
+                VerticalVelocity = Gravity * 0.3f;
 
-        //중력 적용
-        if(cc.isGrounded == false)
-            VerticalVelocity = Gravity;
+            MovementVelocity += VerticalVelocity * Vector3.up * Time.deltaTime;
+
+            cc.Move(MovementVelocity);
+        }
         else
-            VerticalVelocity = Gravity*0.3f;
-
-        MovementVelocity += VerticalVelocity * Vector3.up * Time.deltaTime;
-
-        cc.Move(MovementVelocity);
+            CalculateEnemyMovement();
+       
     }
 }
 
